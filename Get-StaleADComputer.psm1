30d3e1c26233a4ffc250adf3bf164c81eb820555
DataKrashKorrupt <m4rc.g@pm.me>
2023-11-09 17:50:02 +1100

    >Added comment based help >Need to test in AD lab after changes made

--- Get-StaleADComputer.psm1
+++ Get-StaleADComputer.psm1
@@ -1,4 +1,46 @@
 function Get-StaleADComputer {
+<#
+.SYNOPSIS
+This tool will query AD for enabled computer objects that have not authenticated with the Domain Controller within the specified timeframe of either months or years.
+
+.DESCRIPTION
+A simple tool that queries Active Directory for active computer objects that have not authenticated with the Domain Controller within a specified time period. This module must be either run locally on Domain Controller or on endpoint with the ActiveDirectory module installed to run the query.
+
+The tool uses LastLogonDate property of the computer object to determine the results returned based on input.
+
+.PARAMETER LastLoginMonths
+Enter number of months (1-11) in past time as threshold. Will return any object with a login time stamp older than this.
+
+.PARAMETER LastLoginYears
+Enter number of years in past time as threshold. Will return any object with a login time stamp older than this.
+
+.EXAMPLE
+This will return computer objects that have not authenticated >6 months, will sort objects by LastLogon timestamp with oldest at the top, and export the results to a CSV file:
+
+Get-StaleADComputer -LastLoginMonths 6 | Sort-Object LastLogon | Export-CSV C:\temp\staleADComputers.csv
+
+.EXAMPLE
+This will return all computer objects that have not authenticated in >1 year (default value) and display results in table in the console:
+
+Get-StaleADComputer -LastLoginYears 
+
+.EXAMPLE
+Will return all computer objects that have not authenticated in >2 months (positional parameter in default parameter set) and will display results in table in the console with oldest at the top:
+
+Get-StaleADComputer 2 | Sort-Object LastLogon
+
+.INPUTS
+None. You cannot pipe objects to this module.
+
+.OUTPUTS
+PSObject
+
+.LINK
+https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps
+
+.LINK
+https://github.com/DataKrashKorrupt/Get-StaleADComputer.git
+#>
     [CmdletBinding(DefaultParameterSetName="Months")]
     param (
         [Parameter(ParameterSetName="Years")]
