edaff7ab2cbc87ebe10e8e8d0701ede438e3653b
DataKrashKorrupt <github.craftwork373@passmail.net>
2024-02-08 20:12:25 +1100

    > Added external help > Removed comment based help

--- FindStaleADObjects.psm1
+++ FindStaleADObjects.psm1
@@ -1,44 +1,4 @@
 function Get-StaleADComputer {
-<#
-.SYNOPSIS
-This tool will query AD for enabled computer objects that have not authenticated with the Domain Controller within the specified timeframe of either months or years.
-
-.DESCRIPTION
-A simple tool that queries Active Directory for active computer objects that have not authenticated with the Domain Controller within a specified time period. This module must be either run locally on Domain Controller or on endpoint with the ActiveDirectory module installed to run the query.
-
-The tool uses LastLogonDate property of the computer object to determine the results returned based on input.
-
-.PARAMETER LastLoginMonths
-Enter number of months (1-11) in past time as threshold. Will return any object with a login time stamp older than this.
-
-.PARAMETER LastLoginYears
-Enter number of years in past time as threshold. Will return any object with a login time stamp older than this.
-
-.EXAMPLE
-This will return computer objects that have not authenticated in over 6 months, will sort objects by LastLogon timestamp with oldest at the top, and export the results to a CSV file:
-
-Get-StaleADComputer -LastLoginMonths 6 | Sort-Object LastLogon | Export-CSV C:\temp\staleADComputers.csv
-
-.EXAMPLE
-This will return all computer objects that have not authenticated in over 1 year (default value) and display results in table in the console:
-
-Get-StaleADComputer -LastLoginYears 
-
-.EXAMPLE
-Will return all computer objects that have not authenticated in over 2 months (positional parameter in default parameter set) and will display results in table in the console with oldest at the top:
-
-Get-StaleADComputer 2 | Sort-Object LastLogon
-
-.INPUTS
-None. You cannot pipe objects to this module.
-
-.OUTPUTS
-PSObject
-
-.LINK
-https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps
-
-#>
     [CmdletBinding(DefaultParameterSetName="Months")]
     param (
         [Parameter(ParameterSetName="Years")]
@@ -77,46 +37,6 @@ https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=window
 
 
 function Get-StaleADUser {
-<#
-.SYNOPSIS
-This tool will query AD for enabled user objects that have not authenticated with the Domain Controller within the specified timeframe of either months or years.
-
-.DESCRIPTION
-A simple tool that queries Active Directory for active user objects that have not authenticated with the Domain Controller within a specified time period. This module must be either run locally on Domain Controller or on endpoint with the ActiveDirectory module installed to run the query.
-
-The tool uses LastLogonDate property of the user object to determine the results returned based on input.
-
-.PARAMETER LastLoginMonths
-Enter number of months (1-11) in past time as threshold. Will return any object with a login time stamp older than this.
-
-.PARAMETER LastLoginYears
-Enter number of years in past time as threshold. Will return any object with a login time stamp older than this.
-
-.EXAMPLE
-This will return user objects that have not authenticated >6 months, will sort objects by LastLogon timestamp with oldest at the top, and export the results to a CSV file:
-
-Get-StaleADuser -LastLoginMonths 6 | Sort-Object LastLogon | Export-CSV C:\temp\staleADusers.csv
-
-.EXAMPLE
-This will return all user objects that have not authenticated in >1 year (default value) and display results in table in the console:
-
-Get-StaleADuser -LastLoginYears 
-
-.EXAMPLE
-Will return all user objects that have not authenticated in >2 months (positional parameter in default parameter set) and will display results in table in the console with oldest at the top:
-
-Get-StaleADuser 2 | Sort-Object LastLogon
-
-.INPUTS
-None. You cannot pipe objects to this module.
-
-.OUTPUTS
-PSObject
-
-.LINK
-https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps
-
-#>
     [CmdletBinding(DefaultParameterSetName='Months')]
     param (
         [Parameter(ParameterSetName='Years')]
